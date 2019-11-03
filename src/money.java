544d0eb40b7a9b0837225d58ee96226c80cb3e59
jonathanaas <jalvesalonso@hotmail.com>
2019-11-03 16:40:54 -0200

    Update money.java

--- src/money.java
+++ src/money.java
@@ -40,11 +40,6 @@ public void testMultiplication() {
    assertEquals(new Dollar(15), five.times(3));
 }
 
-public void testEquality() {
-   assertTrue(new Dollar(5).equals(new Dollar(5)));
-   assertFalse(new Dollar(5).equals(new Dollar(6)));
-}
-
 public void testFrancMultiplication() {
    Franc five = new Franc(5);
    assertEquals(new Franc(10), five.times(2));
