1b895a7f5c96c8ab7b06365a8ad0a84c2b91e302
jonathanaas <jalvesalonso@hotmail.com>
2019-11-03 17:04:21 -0200

    Update money.java

--- src/money.java
+++ src/money.java
@@ -1,6 +1,7 @@
 // Class Money
-class Money  {
+abstract class Money  {
    protected int amount;
+   protected String currency;
    
    public boolean equals(Object object){
    	Money money = (Money) object;
@@ -8,37 +9,46 @@ class Money  {
    }
 
 	static Money dollar(int amount){
-  		return new Dollar(amount);
+  		return new Dollar(amount, "USD");
 	}
 
 	static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
 
-	abstract Money times(int multiplier);   
+	Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
+    }
+
+	abstract Money times(int multiplier);
+	
+	String currency(){
+		return currency;
+	}   
 }
 
 // Classe Dollar
 class Dollar extends Money {
 
-  Dollar(int amount){
-  	this.amount = amount;
+  Dollar(int amount, String currency){
+  	super(amount, currency);
   }
 
   Money times(int multiplier){
-  	return new Dollar(amount * multiplier);
-  }
+  	return Money.dollar(amount * multiplier);
+  }  
 }
 
 // Classe Franc
 class Franc extends Money {
 
-  Franc(int amount){
-  	this.amount = amount;
+  Franc(int amount, String currency){
+  	super(amount, currency);
   }
 
   Money times(int multiplier){
-  	return new Franc(amount * multiplier);
+  	return Money.franc(amount * multiplier);
   }
 }
 
@@ -61,4 +71,9 @@ public void testEquality(){
    assertTrue(Money.franc(5).equals(Money.franc(5)));
    assertFalse(Money.franc(5).equals(Money.franc(6)));
    assertFalse(Money.franc(5).equals(Money.dollar(5)));
+}
+
+public void testCurrency() {
+   assertEquals("USD", Money.dollar(1).currency());
+   assertEquals("CHF", Money.franc(1).currency());
 }
